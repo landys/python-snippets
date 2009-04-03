79afc4729fc8917a84b899cba91e662418dd233c
landys <landys.cn@gmail.com>
2009-04-03 08:05:46 +0000

    Modify the setup.py.

--- CsdnWz2Atom/trunk/src/tony/cw2atom/setup.py
+++ CsdnWz2Atom/trunk/src/tony/cw2atom/setup.py
@@ -1,17 +1,18 @@
+# the command to sun setup: python setup.py py2exe
 from distutils.core import setup  
 import py2exe  
-includes = ["encodings", "encodings.*"]  
+#includes = ["encodings", "encodings.*"]  
 options = {"py2exe":  
             {   "compressed": 1,  
                 "optimize": 2,  
-                "includes": includes,  
-                "bundle_files": 1  
+#                "includes": includes,  
+                "bundle_files": 1  # generate only .exe file containing all things.
             }  
           }  
 setup(     
     version = "0.1.0",  
-    description = "search panda",  
-    name = "search panda",  
+    description = "csdn cw to atom xml file.",  
+    name = "cw2atom",  
     options = options,  
     zipfile=None,  
     #windows=[{"script": "cw2atom.py", "icon_resources": [(1, "cw2atom.ico")] }],    
