1ccff3887fda639cffe9b0efbebf4153276dd617
landys <landys.cn@gmail.com>
2009-04-11 13:35:31 +0000

    Initial import.

---
+++ atom2bm/trunk/src/tony/atom2bm/setup.py
@@ -0,0 +1,20 @@
+# the command to sun setup: python setup.py py2exe
+from distutils.core import setup  
+import py2exe  
+#includes = ["encodings", "encodings.*"]  
+options = {"py2exe":  
+            {   "compressed": 1,  
+                "optimize": 2,  
+#                "includes": includes,  
+                "bundle_files": 1  # generate only .exe file containing all things.
+            }  
+          }  
+setup(     
+    version = "0.1.0",  
+    description = "atom xml to bookmark html file for delicious.",  
+    name = "atom2bm",  
+    options = options,  
+    zipfile=None,  
+    #windows=[{"script": "atom2bm.py", "icon_resources": [(1, "atom2bm.ico")] }],    
+    console=[{"script": "atom2bm.py", "icon_resources": [(1, "atom2bm.ico")] }],   
+    )  
