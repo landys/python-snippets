f46ea70e7f88e701062e202ecd318f780bac3776
landys <landys.cn@gmail.com>
2009-06-03 11:16:32 +0000

    Fix bug in exe generated by py2exe: Traceback (most recent call last):   File "atom2bm.py", line 92, in <module>   File "atom2bm.py", line 55, in go   File "atom2bm.py", line 66, in printBookmark ImportError: No module named _strptime

--- atom2bm/trunk/src/tony/atom2bm/setup.py
+++ atom2bm/trunk/src/tony/atom2bm/setup.py
@@ -2,9 +2,11 @@
 from distutils.core import setup  
 import py2exe  
 #includes = ["encodings", "encodings.*"]  
+# package _strptime fixed run error in exe: "ImportError: No module named _strptime" 
 options = {"py2exe":  
             {   "compressed": 1,  
-                "optimize": 2,  
+                "optimize": 2, 
+                "packages": ["_strptime"],
 #                "includes": includes,  
                 "bundle_files": 1  # generate only .exe file containing all things.
             }  
